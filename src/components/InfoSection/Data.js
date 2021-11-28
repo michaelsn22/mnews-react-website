bd106fa70dba73f8c79a8d8706112531ce84385b
michaelsn22 <michael.newman@sjsu.edu>
2021-11-27 21:52:50 -0800

    Changed from default react to custom react project
    Changed the code from default react boiler plate code to custom react project code that I am working on.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,67 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Those key details',
+    headLine: 'Get to know a little more about us',
+    description: 'Whats up guys its mnew here and in todays video',
+    buttonLabel: 'About Me',
+    imgStart: true,
+    img: require("../../images/svg-6.svg").default,
+    alt: 'Programmer on desk',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Find me on linked-in and other socials',
+    headLine: 'Get in contact with me!',
+    description: '',
+    buttonLabel: 'Socials',
+    imgStart: false,
+    img: require("../../images/svg-1.svg").default,
+    alt: 'Programming',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'services',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Check out some of the projects I have!',
+    headLine: 'Im on GitHub!',
+    description: '',
+    buttonLabel: 'GitHub',
+    imgStart: true,
+    img: require("../../images/svg-5.svg").default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjFour = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Sign up with us',
+    headLine: 'Subscribe to our mailing list to be updated on new projects!',
+    description: '',
+    buttonLabel: 'Subscribe',
+    imgStart: false,
+    img: require("../../images/svg-3.svg").default,
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: false
+};
